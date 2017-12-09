5b66dcdc9f1f250334e4791dc3e5ad58c04d4b73
SolarStreak <33379170+SolarStreak@users.noreply.github.com>
2017-12-09 11:05:51 -0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+function login() {
+Var username=document.getElementById('username').value;
+Var password=document.getElementById('password').value;
+
+If (username=='Chloe' && password=='Solar'){
+	window.location.href='home.html';
+}
