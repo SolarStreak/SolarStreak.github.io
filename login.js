e3079f1f93ec39f0906674621cb4837ed38c29bc
SolarStreak <33379170+SolarStreak@users.noreply.github.com>
2017-12-16 10:25:42 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function login() {
 var username=document.getElementById('username').value;
 var password=document.getElementById('password').value;
-
+alert(username+" "+password);
 if (username==='Solar' && password==='Streak'){
 	window.location.href='home.html';
 }
